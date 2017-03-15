ca0177ffe82f77b5803ab4f96a298474458ff8e8
Ming,Hsaun_Hsu <f259010@gmail.com>
2017-03-16 00:29:43 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Your Javascript Code Goes Here
-var name = "DTD";
-var age = 10;
-console.log("My name is " + name + ", and My age is " + age);
-
-name = "Hsuan"
-age = 21;
-
-console.log(`My name is ${name}, and my age is ${age}`)
