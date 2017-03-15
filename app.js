c44cfe4209c28bdd603ba819027ea196a73d5646
Ming,Hsaun_Hsu <f259010@gmail.com>
2017-03-16 00:18:22 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "DTD";
+var age = 10;
+console.log("My name is " + name + ", and My age is " + age);
+
+name = "Hsuan"
+age = 21;
+
+console.log(`My name is ${name}, and my age is ${age}`)
