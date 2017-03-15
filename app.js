4c614fd51e0921f8ad47e6b3bd7e070e206a6664
Ming,Hsaun_Hsu <f259010@gmail.com>
2017-03-16 00:26:43 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Your Javascript Code Goes Here
+var name = "DTD";
+var age = 10;
+console.log("My name is " + name + ", and My age is " + age);
+
+name = "Hsuan"
+age = 21;
+
+console.log(`My name is ${name}, and my age is ${age}`)
